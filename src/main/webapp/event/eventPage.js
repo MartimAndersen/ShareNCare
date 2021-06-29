2176d6a281ff99ca0056ad5fae49990bde7dd756
Carolina <cp.duarte@campus.fct.unl.pt>
2021-06-29 17:54:33 +0100

    events web (not tested)

---
+++ src/main/webapp/event/eventPage.js
@@ -0,0 +1,48 @@
+
+
+
+function callChangeAttributes(data) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            switch (this.status) {
+                case 200: alert(this.responseText); break;
+                case 406: alert("Wrong parameters."); break;
+                default: alert("Something went wrong"); break;
+            }
+        }
+    };
+    xhttp.open("POST", "/rest/event/registerEvent", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(data);
+}
+
+
+function handleCreateEvent() {
+    let inputs = document.getElementsByName("createEventInput")
+    let data = {
+        name: inputs[0].value,
+        description: inputs[1].value,
+        minParticipants: inputs[2].value,
+        maxParticipants: inputs[3].value,
+        lat: inputs[4].value,
+        lon: inputs[5].value,
+        date: inputs[6].value,
+        temporary: inputs[6].value,
+        tags : ""
+
+    }
+    callCreateEvent(JSON.stringify(data));
+}
+
+
+
+let changeAttributesForm = document.getElementById("changeAttributesId");
+changeAttributesForm.onsubmit = () => {
+    handleCreateEvent();
+    return false;
+}
+
+function goBack() {
+    window.location.href = "../afterLogin/afterLogin.html";
+}
