19cc0db2eddaa97d1d3a1259566f73dc3dee9931
Martim Andersen <martimandersen@gmail.com>
2021-06-30 17:17:04 +0100

    start about us page

--- src/main/webapp/afterLogin/afterLoginCompanyPage.js
+++ src/main/webapp/afterLogin/afterLoginCompanyPage.js
@@ -20,6 +20,18 @@ function goToCreateEvent() {
     window.location.href = "functionalities/createEvent/createEvent.html";
 }
 
-function goToLogout() {
-    window.location.href = "functionalities/logout/logout.html";
+function logout() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            switch (this.status) {
+                case 200: alert(this.responseText); backToInitialPage(); break;
+                default: alert("You need to be logged in to execute this operation."); break;
+            }
+        }
+    };
+    xhttp.open("POST", "/rest/loggedIn/logout", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    xhttp.send("");
 }
