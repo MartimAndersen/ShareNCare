83eaa8f906e93c2dffe54ba7cc1007270c2635f0
Carolina <cp.duarte@campus.fct.unl.pt>
2021-07-01 18:46:33 +0100

    joinEvent

--- src/main/webapp/afterLogin/functionalities/joinEvent/joinEvent.js
+++ src/main/webapp/afterLogin/functionalities/joinEvent/joinEvent.js
@@ -2,28 +2,65 @@ function goToPageBefore(){
     window.location.href = "../../afterLoginPage.html";
 }
 
+function myFunction(jsonResponse) {
+    for(var i = 0; i < jsonResponse.length; i++) {
+    var obj = []
+         obj= JSON.parse(jsonResponse[i]);
+
+        console.log(obj);
+    }
+}
+function populate_table(jsonResponse) {
+   let table = document.getElementById('demo_table');
+   for(var i = 0; i < jsonResponse.length; i++) {
+      var obj = []
+             obj= JSON.parse(jsonResponse[i]);
+        let row = table.insertRow(-1);
+		let cell = row.insertCell(0);
+		let text = document.createTextNode((obj[7].value));
+        cell.appendChild(text);
+		cell = row.insertCell(0);
+		text = document.createTextNode((obj[4].value));
+        cell.appendChild(text);
+		cell = row.insertCell(0)
+		text = document.createTextNode((obj[3].value));
+        cell.appendChild(text);
+        cell = row.insertCell(0)
+         text = document.createTextNode((obj[2].value));
+        cell.appendChild(text);
+        cell = row.insertCell(0)
+         text = document.createTextNode((obj[1].value));
+        cell.appendChild(text);
+        cell = row.insertCell(0)
+        text = document.createTextNode((obj[5].value));
+        cell.appendChild(text);
+   }
+}
+
 function callGetEvents(){
+ var jsonResponse = []
 let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
            //console.log(xhttp.response);
           // console.log(xhttp.responseText);
-          var jsonResponse = JSON.parse(xhttp.responseText);
-          console.log(jsonResponse);
+          jsonResponse = JSON.parse(xhttp.responseText);
+         populate_table(jsonResponse)
     }
+
 };
 xhttp.open("GET", "/rest/event/getAllEventsWeb", true);
 xhttp.send();
 
-var jsonResponse = JSON.parse(xhttp.responseText);
-console.log(jsonResponse);
 
+/*
 let names = []
-_.each(jsonResponse, (result) => {
+jsonResponse.forEach(jsonResponse, (result) => {
     names.push(result.name)
 })
 console.log(names);
+*/
 
 
 
