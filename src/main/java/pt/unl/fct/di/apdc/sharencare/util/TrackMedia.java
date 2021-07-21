98ac71e1781099173ddf8c911b70f6e39243a98b
Maria Gomes <mf.gomes@campus.fct.unl.pt>
2021-07-21 18:32:11 +0100

    uri to string

--- src/main/java/pt/unl/fct/di/apdc/sharencare/util/TrackMedia.java
+++ src/main/java/pt/unl/fct/di/apdc/sharencare/util/TrackMedia.java
@@ -6,12 +6,12 @@ import com.google.cloud.datastore.LatLng;
 
 public class TrackMedia {
 
-    public Uri imageUri;
+    public String imageUri;
     public LatLng location;
     public String username;
     public int likes;
 
-    public TrackMedia(Uri imageUri, String username, LatLng location, int likes) {
+    public TrackMedia(String imageUri, String username, LatLng location, int likes) {
         this.imageUri = imageUri;
         this.location = location;
         this.username = username;
@@ -24,14 +24,6 @@ public class TrackMedia {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Uri getImageUri() {
-        return imageUri;
-    }
-
-    public void setImageUri(Uri imageUri) {
-        this.imageUri = imageUri;
     }
 
     public LatLng getLocation() {
